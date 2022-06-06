bc5c594276498d4f25622e62f49775b6a3774e42
Jarek Pietrasina <j.pietrasina@eikona-media.de>
2022-06-06 21:34:48 +0200

    edit highlight card component

--- templates/theme/rsce_highlight_card_config.php
+++ templates/theme/rsce_highlight_card_config.php
@@ -10,6 +10,75 @@ return [
     'moduleCategory' => 'yatelier',
     'standardFields' => ['cssID'],
     'fields' => [
+        'align' => [
+            'label' => ['Anordnung Desktop', ''],
+            'inputType' => 'select',
+            'options' => [
+                't-t' => 'Text on top (center)',
+                't-b' => 'Text on bottom (center)',
+            ],
+            'eval' => ['tl_class' => 'w50 clr'],
+        ],
+        'inverted' => [
+            'label' => ['Weiße Schrift', ''],
+            'inputType' => 'checkbox',
+            'eval' => ['tl_class' => 'w50 m12'],
+            'default' => 0,
+        ],
+        'icon' => [
+            'label' => [
+                'en' => ['Icon', ''],
+                'de' => ['Icon', ''],
+            ],
+            'inputType' => 'fileTree',
+            'eval' => [
+                'tl_class' => 'clr ',
+                'fieldType' => 'radio',
+                'filesOnly' => true,
+                'extensions' => Config::get('validImageTypes'),
+            ],
+        ],
+
+        'tagline' => [
+            'label' => [
+                'en' => ['Tagline', ''],
+                'de' => ['Tagline', ''],
+            ],
+            'inputType' => 'text',
+            'eval' => ['tl_class' => 'w50'],
+        ],
+
+        'claim' => [
+            'label' => [
+                'en' => ['Claim', 'Beispiel: Nike - Just do it. (optional)'],
+                'de' => ['Claim', 'Beispiel: Nike - Just do it. (optional)'],
+            ],
+            'inputType' => 'text',
+            'eval' => ['tl_class' => 'w50'],
+        ],
+
+        'description' => [
+            'label' => [
+                'en' => ['Description', ''],
+                'de' => ['Beschreibung', ''],
+            ],
+            'inputType' => 'textarea',
+            'eval' => ['rte' => 'tinyMCE', 'mandatory' => false, 'tl_class' => ' clr'],
+        ],
+
+        'backgroundImage' => [
+            'label' => [
+                'en' => ['Background-Image', ''],
+                'de' => ['Hintergrund-Bild', ''],
+            ],
+            'inputType' => 'fileTree',
+            'eval' => [
+                'tl_class' => 'clr w50',
+                'fieldType' => 'radio',
+                'filesOnly' => true,
+                'extensions' => Config::get('validImageTypes'),
+            ],
+        ],
         'size' => [
             'label' => $GLOBALS['TL_LANG']['tl_content']['size'],
             'inputType' => 'imageSize',
@@ -24,121 +93,37 @@ return [
             'options' => Contao\System::getContainer()->get('contao.image.image_sizes')->getAllOptions(),
         ],
 
-        'singleSRC' => [
-            'label' => $GLOBALS['TL_LANG']['tl_content']['singleSRC'],
-            'inputType' => 'fileTree',
-            'eval' => [
-                'filesOnly' => true,
-                'fieldType' => 'radio',
-                'mandatory' => false,
-                'tl_class' => 'w50 clr',
-                'extensions' => Contao\Config::get('validImageTypes'),
-            ],
+        'url' => [
+            'label' => $GLOBALS['TL_LANG']['MSC']['url'],
+            'inputType' => 'url',
+            'eval' => ['tl_class' => 'clr w50 wizard'],
         ],
-        'headline' => [
-            'label' => $GLOBALS['TL_LANG']['tl_content']['headline'],
-            'standardFields' => ['headline'],
+        'target' => [
+            'label' => $GLOBALS['TL_LANG']['MSC']['target'],
+            'inputType' => 'checkbox',
+            'eval' => ['tl_class' => 'w50 m12'],
+        ],
+        'linkTitle' => [
+            'label' => $GLOBALS['TL_LANG']['tl_content']['linkTitle'],
             'inputType' => 'text',
-            'eval' => [
-                'tl_class' => 'w50 clr',
-                'mandatory' => true
-            ]
+            'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
         ],
-        'price' => [
-            'label' => [
-                'en' => ['Price', ''],
-                'de' => ['Preis', ''],
-            ],
+        'titleText' => [
+            'label' => $GLOBALS['TL_LANG']['tl_content']['titleText'],
             'inputType' => 'text',
-            'eval' => ['tl_class' => 'w50'],
+            'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
         ],
-        'myColor' => [
-            'label' => ['Farbauswähl', 'Standardmäßig grau von yatelier'],
-            'inputType' => 'text',
-            'eval' => [
-                'maxlength' => 6,
-                'multiple' => false,
-                'size' => 2,
-                'colorpicker' => true,
-                'isHexColor' => true,
-                'decodeEntities' => true,
-                'tl_class' => 'w50 wizard'
+        'buttonType' => [
+            'label' => ['Button-Type'],
+            'inputType' => 'select',
+            'options' => [
+                'btn--primary' => 'Primary',
+                'btn--secondary' => 'Secondary',
+                'btn--inverted' => 'Inverted',
             ],
+            'eval' => ['tl_class' => 'w50 clr'],
         ],
-        'links' => [
-            'label' => [
-                'en' => ['Button Links', ''],
-                'de' => ['Button Links', ''],
-            ],
-            'elementLabel' => 'Button %s',
-            'inputType' => 'list',
-            'maxItems' => 2,
-            'fields' => [
-                'url' => [
-                    'label' => $GLOBALS['TL_LANG']['MSC']['url'],
-                    'inputType' => 'url',
-                    'eval' => ['tl_class' => 'clr w50 wizard'],
-                ],
-                'target' => [
-                    'label' => $GLOBALS['TL_LANG']['MSC']['target'],
-                    'inputType' => 'checkbox',
-                    'eval' => ['tl_class' => 'w50 m12'],
-                ],
-                'linkTitle' => [
-                    'label' => $GLOBALS['TL_LANG']['tl_content']['linkTitle'],
-                    'inputType' => 'text',
-                    'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
-                ],
-                'titleText' => [
-                    'label' => $GLOBALS['TL_LANG']['tl_content']['titleText'],
-                    'inputType' => 'text',
-                    'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
-                ],
-                'buttonType' => [
-                    'label' => ['Button-Type'],
-                    'inputType' => 'select',
-                    'options' => [
-                        'btn--primary' => 'Primary',
-                        'btn--secondary' => 'Secondary',
-                        'btn--inverted' => 'Inverted',
-                    ],
-                    'eval' => ['tl_class' => 'w50 clr'],
-                ],
-                'ArrowIcon' => [
-                    'label' => ['Button mit Pfeil'],
-                    'inputType' => 'checkbox',
-                    'eval' => ['tl_class' => 'w50 m12'],
-                ],
-            ],
-        ],
+
     ],
-    'grid_legend' => [
-        'label' => ['Grid-Einstellungen', 'Für alle Elemente'],
-        'inputType' => 'group',
-    ],
-    'grid_columns' => [
-        'label' => &$GLOBALS['TL_LANG']['tl_content']['grid_columns'],
-        'inputType' => 'select',
-        'options_callback' => ['GridClass', 'getGridCols'],
-        'eval' => [
-            'mandatory' => false,
-            'multiple' => true,
-            'size' => 10,
-            'tl_class' => 'clr w50 w50h autoheight',
-            'chosen' => true,
-        ],
-        'default' => ['col-md-6'],
-    ],
-    'grid_options' => [
-        'label' => &$GLOBALS['TL_LANG']['tl_content']['grid_options'],
-        'inputType' => 'select',
-        'options_callback' => ['GridClass', 'getGridOptions'],
-        'eval' => [
-            'mandatory' => false,
-            'multiple' => true,
-            'size' => 10,
-            'tl_class' => 'w50 w50h autoheight',
-            'chosen' => true,
-        ],
-    ],
+
 ];
